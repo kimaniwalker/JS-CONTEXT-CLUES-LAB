29ec63e861101da20a62a981a19ddcb5433ecd04
Kimani Walker <kimaniwalker@gmail.com>
2018-06-07 15:04:34 -0500

    Added function

--- script.js
+++ script.js
@@ -15,29 +15,26 @@ document.addEventListener("DOMContentLoaded", function () {
     //Weapon Array 
     var weaponarray = ["Knife", "Grenade", "Pencil", "Hammer", "Fist", "gun", "Shotgun", "Pistol", "Rifle", "Table", "Knife2", "Grenade2", "Pencil2", "Hammer2", "Fist2", "gun2", "Shotgun2", "Pistol2", "Rifle2", "Table2"]
 
+    accusationlist();
+    function accusationlist() {
+        var i;
+
+        for (i = 1; i < 101; i++) {
 
 
+            var h3 = document.createElement('h3');
+            var h3Text = document.createTextNode('Accusations' + i);
+            h3.appendChild(h3Text);
+            document.body.appendChild(h3);
 
-
-    var i;
-
-    for (i = 1; i < 101; i++) {
-
-
-        var h3 = document.createElement('h3');
-        var h3Text = document.createTextNode('Accusations' + i);
-        h3.appendChild(h3Text);
-        document.body.appendChild(h3);
-
-        h3.addEventListener("click", function () {
-            alert('I accuse ' + [friendarray] + ', with the ' + [weaponarray] + ' in the ' + [locationarray]);
-
-
-
-
-        })
-
-
+            h3.addEventListener("click", function () {
+                alert('I accuse ' + [friendarray] + ', with the ' + [weaponarray] + ' in the ' + [locationarray]);
+            console.log(i);
+            })
+           
+        }
+        
+        
     }
 
 })
