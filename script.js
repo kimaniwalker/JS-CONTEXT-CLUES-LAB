5819c5730e6a7d27c54fab8b7c998223377e60ac
Kimani Walker <kimaniwalker@gmail.com>
2018-06-07 10:31:53 -0500

    CREATE H3 TEXT ELEMENTS

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-console.log('Test');
+console.log('test');
 
+document.addEventListener("DOMContentLoaded", function () {
 // When the page first loads, insert 100 h3 elements onto the page 
 // The h3 elements should simply say Accusation 1, Accusation 2, Accusation 3, ... Accusation 100
-
-var h3 = document.createElement('h3');
+// Create h3 element
+//Create Text Element
+//Put text in h3
+//Put text in body
+    var h3 = document.createElement('h3');
         var h3Text = document.createTextNode("Accusation");
         h3.appendChild(h3Text);
         document.body.appendChild(h3);
+
+        console.log(h3);
+
+})
