4160a81eff7cac1ee87b6070ad6272f1d16be539
Kimani Walker <kimaniwalker@gmail.com>
2018-06-07 15:11:22 -0500

    Cleaned up code / deleted unneeded comments

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 console.log('test');
 document.addEventListener("DOMContentLoaded", function () {
-    // When the page first loads, insert 100 h3 elements onto the page 
-    // The h3 elements should simply say Accusation 1, Accusation 2, Accusation 3, ... Accusation 100
-
-
+    
     //Arrays
 
     //Friend Array
